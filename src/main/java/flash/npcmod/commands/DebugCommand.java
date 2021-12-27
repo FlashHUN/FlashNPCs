f4041d29179867a85a34eb5b540746b2418b0e16
FlashHUN <molnarkocka@gmail.com>
2021-12-27 03:47:18 +0100

    feat: added debug command

---
+++ src/main/java/flash/npcmod/commands/DebugCommand.java
@@ -0,0 +1,87 @@
+package flash.npcmod.commands;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import flash.npcmod.capability.quests.IQuestCapability;
+import flash.npcmod.capability.quests.QuestCapabilityProvider;
+import flash.npcmod.core.quests.Quest;
+import flash.npcmod.core.quests.QuestInstance;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.util.ArrayList;
+
+import static net.minecraft.commands.Commands.literal;
+import static net.minecraft.commands.Commands.argument;
+
+public class DebugCommand extends Command {
+
+  @Override
+  public String getName() {
+    return "debug";
+  }
+
+  @Override
+  public int getRequiredPermissionLevel() {
+    return 4;
+  }
+
+  @Override
+  public void build(LiteralArgumentBuilder<CommandSourceStack> builder) {
+    builder.then(argument("player", EntityArgument.player())
+            .then(literal("capability")
+                    .then(literal("quests")
+                            .then(literal("tracked").executes(context -> tracked(context.getSource(), EntityArgument.getPlayer(context, "player"))))
+                            .then(literal("accepted").executes(context -> accepted(context.getSource(), EntityArgument.getPlayer(context, "player"))))
+                            .then(literal("completed").executes(context -> completed(context.getSource(), EntityArgument.getPlayer(context, "player"))))
+                    )
+            )
+    );
+  }
+
+  @Override
+  public boolean isDedicatedServerOnly() {
+    return false;
+  }
+
+  private int tracked(CommandSourceStack source, ServerPlayer player) {
+    IQuestCapability capability = QuestCapabilityProvider.getCapability(player);
+    source.sendSuccess(new TextComponent(player.getName().getString() + "'s tracked quest: " + (capability.getTrackedQuest() == null ? "" : capability.getTrackedQuest())), false);
+    return 1;
+  }
+
+  private int accepted(CommandSourceStack source, ServerPlayer player) {
+    IQuestCapability capability = QuestCapabilityProvider.getCapability(player);
+    source.sendSuccess(new TextComponent(player.getName().getString() + "'s accepted quests:"), false);
+
+    ArrayList<QuestInstance> accepted = capability.getAcceptedQuests();
+    if (accepted.size() > 0) {
+      for (QuestInstance questInstance : accepted) {
+        Quest quest = questInstance.getQuest();
+        source.sendSuccess(new TextComponent("- " + quest.getDisplayName() + " [" +quest.getName() + "]"), false);
+      }
+      return 1;
+    }
+    return 0;
+  }
+
+  private int completed(CommandSourceStack source, ServerPlayer player) {
+    IQuestCapability capability = QuestCapabilityProvider.getCapability(player);
+    source.sendSuccess(new TextComponent(player.getName().getString() + "'s completed quests:"), false);
+
+    ArrayList<String> completed = capability.getCompletedQuests();
+    if (completed.size() > 0) {
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < completed.size(); i++) {
+        sb.append(completed.get(i));
+        if (i < completed.size()-1) {
+          sb.append(", ");
+        }
+      }
+      source.sendSuccess(new TextComponent(sb.toString()), false);
+      return 1;
+    }
+    return 0;
+  }
+}
