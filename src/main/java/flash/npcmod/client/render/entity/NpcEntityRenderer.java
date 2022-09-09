647f5a8cc4b05dbddf73a176e6b012767f1da220
FlashHUN <molnarkocka@gmail.com>
2022-09-09 16:46:16 +0200

    feat: custom model rendering - still have to figure out layerrenderers

--- src/main/java/flash/npcmod/client/render/entity/NpcEntityRenderer.java
+++ src/main/java/flash/npcmod/client/render/entity/NpcEntityRenderer.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import flash.npcmod.core.client.SkinUtil;
 import flash.npcmod.entity.NpcEntity;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
@@ -25,6 +26,7 @@ import net.minecraft.world.entity.Pose;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
 
 @OnlyIn(Dist.CLIENT)
 public class NpcEntityRenderer extends LivingEntityRenderer<NpcEntity, PlayerModel<NpcEntity>> {
@@ -50,12 +52,22 @@ public class NpcEntityRenderer extends LivingEntityRenderer<NpcEntity, PlayerMod
     slimModel = new PlayerModel<>(context.bakeLayer(ModelLayers.PLAYER_SLIM), true);
   }
 
+  private boolean shouldRenderAsNormalNpc(NpcEntity npcEntity) {
+    return npcEntity.getRendererType().equals(npcEntity.getType()) || npcEntity.getEntityToRenderAs() == null;
+  }
+
   @Override
   public ResourceLocation getTextureLocation(NpcEntity entity) {
-    try {
-      return SkinUtil.loadSkin(entity.getTexture());
-    } catch (Exception e) {
-      return DefaultPlayerSkin.getDefaultSkin();
+    if (shouldRenderAsNormalNpc(entity)) {
+      try {
+        return SkinUtil.loadSkin(entity.getTexture());
+      } catch (Exception e) {
+        return DefaultPlayerSkin.getDefaultSkin();
+      }
+    }
+    else {
+      LivingEntity entityToRenderAs = entity.getEntityToRenderAs();
+      return this.entityRenderDispatcher.getRenderer(entityToRenderAs).getTextureLocation(entityToRenderAs);
     }
   }
 
@@ -66,7 +78,7 @@ public class NpcEntityRenderer extends LivingEntityRenderer<NpcEntity, PlayerMod
   @Override
   public void render(NpcEntity entityIn, float entityYaw, float partialTicks, PoseStack matrixStackIn,
                      MultiBufferSource bufferIn, int packedLightIn) {
-    if (entityIn.getRendererType().equals(entityIn.getType()) || entityIn.getEntityToRenderAs() == null) {
+    if (shouldRenderAsNormalNpc(entityIn)) {
       if (entityIn.isSlim())
         this.model = slimModel;
       else
@@ -77,10 +89,99 @@ public class NpcEntityRenderer extends LivingEntityRenderer<NpcEntity, PlayerMod
     }
     else {
       LivingEntity entityToRenderAs = entityIn.getEntityToRenderAs();
-      entityToRenderAs.setYBodyRot(entityIn.yBodyRot);
-      entityToRenderAs.setYHeadRot(entityIn.yHeadRot);
-      this.entityRenderDispatcher.render(entityIn.getEntityToRenderAs(), 0.0, 0.0, 0.0, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
+      LivingEntityRenderer<?,?> renderer = (LivingEntityRenderer<?, ?>) this.entityRenderDispatcher.getRenderer(entityToRenderAs);
+      this.renderCustomModel(entityIn, entityToRenderAs, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn, renderer.getModel());
+      //this.entityRenderDispatcher.render(entityIn.getEntityToRenderAs(), 0.0, 0.0, 0.0, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
     }
+  }
+
+  private void renderCustomModel(NpcEntity npcEntity, LivingEntity renderAs, float entityYaw, float partialTicks, PoseStack matrixStackIn,
+                                 MultiBufferSource bufferIn, int packedLightIn, EntityModel model) {
+    if (net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.client.event.RenderLivingEvent.Pre<>(npcEntity, this, partialTicks, matrixStackIn, bufferIn, packedLightIn))) return;
+    matrixStackIn.pushPose();
+    model.attackTime = this.getAttackAnim(npcEntity, partialTicks);
+
+    boolean shouldSit = npcEntity.isPassenger() && (npcEntity.getVehicle() != null && npcEntity.getVehicle().shouldRiderSit());
+    model.riding = shouldSit;
+    model.young = npcEntity.isBaby();
+    float f = Mth.rotLerp(partialTicks, npcEntity.yBodyRotO, npcEntity.yBodyRot);
+    float f1 = Mth.rotLerp(partialTicks, npcEntity.yHeadRotO, npcEntity.yHeadRot);
+    float f2 = f1 - f;
+    if (shouldSit && npcEntity.getVehicle() instanceof LivingEntity livingentity) {
+      f = Mth.rotLerp(partialTicks, livingentity.yBodyRotO, livingentity.yBodyRot);
+      f2 = f1 - f;
+      float f3 = Mth.wrapDegrees(f2);
+      if (f3 < -85.0F) {
+        f3 = -85.0F;
+      }
+
+      if (f3 >= 85.0F) {
+        f3 = 85.0F;
+      }
+
+      f = f1 - f3;
+      if (f3 * f3 > 2500.0F) {
+        f += f3 * 0.2F;
+      }
+
+      f2 = f1 - f;
+    }
+
+    float f6 = Mth.lerp(partialTicks, npcEntity.xRotO, npcEntity.getXRot());
+    if (isEntityUpsideDown(npcEntity)) {
+      f6 *= -1.0F;
+      f2 *= -1.0F;
+    }
+
+    if (npcEntity.getPose() == Pose.SLEEPING) {
+      Direction direction = npcEntity.getBedOrientation();
+      if (direction != null) {
+        float f4 = npcEntity.getEyeHeight(Pose.STANDING) - 0.1F;
+        matrixStackIn.translate((double)((float)(-direction.getStepX()) * f4), 0.0D, (double)((float)(-direction.getStepZ()) * f4));
+      }
+    }
+
+    float f7 = this.getBob(npcEntity, partialTicks);
+    this.setupRotations(npcEntity, matrixStackIn, f7, f, partialTicks);
+    matrixStackIn.scale(-1.0F, -1.0F, 1.0F);
+    this.scale(npcEntity, matrixStackIn, partialTicks);
+    matrixStackIn.translate(0.0D, (double)-1.501F, 0.0D);
+    float f8 = 0.0F;
+    float f5 = 0.0F;
+    if (!shouldSit && npcEntity.isAlive()) {
+      f8 = Mth.lerp(partialTicks, npcEntity.animationSpeedOld, npcEntity.animationSpeed);
+      f5 = npcEntity.animationPosition - npcEntity.animationSpeed * (1.0F - partialTicks);
+      if (npcEntity.isBaby()) {
+        f5 *= 3.0F;
+      }
+
+      if (f8 > 1.0F) {
+        f8 = 1.0F;
+      }
+    }
+
+    model.prepareMobModel(renderAs, f5, f8, partialTicks);
+    model.setupAnim(renderAs, f5, f8, f7, f2, f6);
+    Minecraft minecraft = Minecraft.getInstance();
+    boolean flag = this.isBodyVisible(npcEntity);
+    boolean flag1 = !flag && !npcEntity.isInvisibleTo(minecraft.player);
+    boolean flag2 = minecraft.shouldEntityAppearGlowing(npcEntity);
+    RenderType rendertype = this.getRenderType(npcEntity, flag, flag1, flag2);
+    if (rendertype != null) {
+      VertexConsumer vertexconsumer = bufferIn.getBuffer(rendertype);
+      int i = getOverlayCoords(npcEntity, this.getWhiteOverlayProgress(npcEntity, partialTicks));
+      model.renderToBuffer(matrixStackIn, vertexconsumer, packedLightIn, i, 1.0F, 1.0F, 1.0F, flag1 ? 0.15F : 1.0F);
+    }
+
+    // TODO layerRenderers
+
+    matrixStackIn.popPose();
+    net.minecraftforge.client.event.RenderNameplateEvent renderNameplateEvent = new net.minecraftforge.client.event.RenderNameplateEvent(npcEntity, npcEntity.getDisplayName(), this, matrixStackIn, bufferIn, packedLightIn, partialTicks);
+    net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(renderNameplateEvent);
+    if (renderNameplateEvent.getResult() != net.minecraftforge.eventbus.api.Event.Result.DENY && (renderNameplateEvent.getResult() == net.minecraftforge.eventbus.api.Event.Result.ALLOW || this.shouldShowName(npcEntity))) {
+      this.renderNameTag(npcEntity, renderNameplateEvent.getContent(), matrixStackIn, bufferIn, packedLightIn);
+    }
+    net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.client.event.RenderLivingEvent.Post<>(npcEntity, this, partialTicks, matrixStackIn, bufferIn, packedLightIn));
   }
   
   private void renderPlayerModel(NpcEntity entityIn, float entityYaw, float partialTicks, PoseStack matrixStackIn,
